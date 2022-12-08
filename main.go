b66d07a134b16a3125a0014e26be6a3c59d14130
Taewan Kim <44514409+yuhatown@users.noreply.github.com>
2022-12-08 09:54:33 +0900

    add .env

--- main.go
+++ main.go
@@ -1,27 +1,45 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
 )
 
 func calendarHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, Miracle Calendar"))
 }
 
-func dailyHandler(w http.ResponseWriter, r *http.Request) {
+func routineHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Check your daily routine"))
 }
 
-func dailyMemoHandler(w http.ResponseWriter, r *http.Request) {
+func routineMemoHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Write memo"))
 }
 
 func main() {
+
 	mux := chi.NewRouter()
 	mux.Get("/", calendarHandler)
-	mux.Get("/daily", dailyHandler)
-	mux.Get("/daily/memo", dailyMemoHandler)
+	mux.Get("/daily", routineHandler)
+	mux.Get("/daily/memo", routineMemoHandler)
 	http.ListenAndServe(":8080", mux)
+
+
+	err := godotenv.Load(".env")
+	if err != nil {
+  		log.Fatal("Error loading .env file")
+	}
+
+	db, err := sql.Open("mysql", os.Getenv("DATA_SOURCE_NAME"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 }
