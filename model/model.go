074e020b922bc218446b71e7efd4bce590c5d888
Taewan Kim <44514409+yuhatown@users.noreply.github.com>
2022-12-14 09:37:31 +0900

    create connection.go & add ConnectDB

--- model/model.go
+++ model/model.go
@@ -1,25 +1,13 @@
 package model
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
-	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/joho/godotenv"
 )
-
-func ConnectDB() (*sql.DB, error) {
-	db, err := sql.Open("mysql", os.Getenv("DATA_SOURCE_NAME"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	return db, err
-}
 
 func GetRoutine() {
 	db, err := ConnectDB()
