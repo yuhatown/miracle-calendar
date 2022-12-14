bbbc73f92583394f626bfb0454df4a153c7fcaea
Taewan Kim <44514409+yuhatown@users.noreply.github.com>
2022-12-14 09:13:15 +0900

    add dotenv autoload

--- model/model.go
+++ model/model.go
@@ -7,15 +7,11 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/joho/godotenv/autoload"
 	"github.com/joho/godotenv"
 )
 
 func GetRoutine() {
-	err := godotenv.Load(".env")
-	if err != nil {
-  		log.Fatal("Error loading .env file")
-	}
-
 	db, err := sql.Open("mysql", os.Getenv("DATA_SOURCE_NAME"))
 	if err != nil {
 		log.Fatal(err)
@@ -24,7 +20,7 @@ func GetRoutine() {
 
 
 	var cal [6]string
-	err = db.QueryRow("SELECT date FROM test").Scan(&cal)
+	err = db.QueryRow("SELECT date FROM routine").Scan(&cal)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,73 +28,56 @@ func GetRoutine() {
 }
 
 func PostRoutine(routine, memo string) {
-	err := godotenv.Load(".env")
-	if err != nil {
-  		log.Fatal("Error loading .env file")
-	}
-
 	db, err := sql.Open("mysql", os.Getenv("DATA_SOURCE_NAME"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	
-
-	err = db.QueryRow("INSERT INTO test(date, routine, memo, is_achieved)) VALUES (now(), ?, ?,0)", routine, memo)
+	var cal [6]string
+	err = db.QueryRow("INSERT INTO routine(date, todo, memo, is_achieved)) VALUES (now(), ?, ?,0)", routine, memo).Scan(&cal)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(cal)
 }
 
 func PutRoutine(routine, memo string, is_achieved, id int) {
-	err := godotenv.Load(".env")
-	if err != nil {
-  		log.Fatal("Error loading .env file")
-	}
-
 	db, err := sql.Open("mysql", os.Getenv("DATA_SOURCE_NAME"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	err = db.QueryRow("UPDATE test SET routine=?, memo=?, is_achieved=? WHERE id=?", routine, memo, is_achieved, id)
+	var cal [6]string
+	err = db.QueryRow("UPDATE routine SET todo=?, memo=?, is_achieved=? WHERE id=?", routine, memo, is_achieved, id).Scan(&cal)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(cal)
 }
 
 func DeleteRoutine(id int) {
-	err := godotenv.Load(".env")
-	if err != nil {
-  		log.Fatal("Error loading .env file")
-	}
-
 	db, err := sql.Open("mysql", os.Getenv("DATA_SOURCE_NAME"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	err = db.QueryRow("DELETE FROM test WHERE id=?", id)
+	var cal [6]string
+	err = db.QueryRow("DELETE FROM routine WHERE id=?", id).Scan(&cal)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(cal)
 }
 
 func GetUser() {
-	err := godotenv.Load(".env")
-	if err != nil {
-  		log.Fatal("Error loading .env file")
-	}
-
 	db, err := sql.Open("mysql", os.Getenv("DATA_SOURCE_NAME"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
-
 
 	var cal [6]string
 	err = db.QueryRow("SELECT * FROM user").Scan(&cal)
@@ -109,55 +88,46 @@ func GetUser() {
 }
 
 func PostUser(name string) {
-	err := godotenv.Load(".env")
-	if err != nil {
-  		log.Fatal("Error loading .env file")
-	}
-
 	db, err := sql.Open("mysql", os.Getenv("DATA_SOURCE_NAME"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()	
 
-	err = db.QueryRow("INSERT INTO user(name) VALUES (?)", name)
+	var cal [6]string
+	err = db.QueryRow("INSERT INTO user(name) VALUES (?)", name).Scan(&cal)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(cal)
 }
 
 func PutUser(name string, id int) {
-	err := godotenv.Load(".env")
-	if err != nil {
-  		log.Fatal("Error loading .env file")
-	}
-
 	db, err := sql.Open("mysql", os.Getenv("DATA_SOURCE_NAME"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	err = db.QueryRow("UPDATE user SET name=? WHERE id=?", name, id)
+	var cal [6]string
+	err = db.QueryRow("UPDATE user SET name=? WHERE id=?", name, id).Scan(&cal)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(cal)
 }
 
 func DeleteUser(id int) {
-	err := godotenv.Load(".env")
-	if err != nil {
-  		log.Fatal("Error loading .env file")
-	}
-
 	db, err := sql.Open("mysql", os.Getenv("DATA_SOURCE_NAME"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	err = db.QueryRow("DELETE FROM user WHERE id=?", id)
+	var cal [6]string
+	err = db.QueryRow("DELETE FROM user WHERE id=?", id).Scan(&cal)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(cal)
 }
